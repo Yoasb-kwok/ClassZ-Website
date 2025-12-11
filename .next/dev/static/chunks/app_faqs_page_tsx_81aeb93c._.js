8c68771613aab9961b40cce125e40b4078a115c9
0899905503 <baovo844@gmail.com>
2025-12-12 00:26:11 +0700

    Update FAQ page content, enhance logging, and adjust font manifest. Clean up app paths manifest by removing unused entries and update preview mode keys in prerender manifest.

--- .next/dev/static/chunks/app_faqs_page_tsx_81aeb93c._.js
+++ .next/dev/static/chunks/app_faqs_page_tsx_81aeb93c._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_1d283607._.js",
-  "static/chunks/_1a3c0d6c._.js"
+  "static/chunks/_1a3c0d6c._.js",
+  "static/chunks/node_modules_1d283607._.js"
 ],
     source: "dynamic"
 });
