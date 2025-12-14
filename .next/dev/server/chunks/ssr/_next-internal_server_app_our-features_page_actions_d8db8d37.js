8f04c29e4939d3c67fbb2b43215ea6dba0b07964
BrianNguyen291 <nguyenvanqui291@gmail.com>
2025-12-14 20:06:43 +0800

    Remove .next build files from Git tracking and add .railwayignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_our-features_page_actions_d8db8d37.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/our-features/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_our-features_page_actions_d8db8d37.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oRPq1DZgeyU6LMhAXs4siVDrO8yDz5H7oDtNwbpJ6V4=\"\n}"
